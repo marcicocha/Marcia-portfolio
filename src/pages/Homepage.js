dcb7ae522468b3c3957a1e8b834a7f8d3c28ca12
Marcia Cole <marcigeoca28@gmail.com>
2022-02-03 08:47:26 +0100

    update on marcia

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Intro from "../components/Intro";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+function Homepage() {
+	return (
+		<div>
+			<Header />
+			<Intro />
+			<Footer />
+		</div>
+	);
+}
+export default Homepage;
